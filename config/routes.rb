ce4f9ed910431d551e114ad8fb18e8aa6677f388
Arturo Poleo <arturopoleo@gmail.com>
2017-06-18 17:39:21 -0400

    Created edit_my_article & my_article

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   post 'create_article', to: 'articles#create_article'
   get 'new_user', to:'users#new_user'
   post 'create_user', to: 'users#create_user'
+  get 'my_article/:id', to: 'articles#my_article'
+  get 'edit_my_article/:id', to: 'articles#edit_my_article'
+  post 'edit_my_article/modify_article', to: 'articles#modify_article'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
